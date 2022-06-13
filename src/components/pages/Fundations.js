e5d21caa8650a21fdafa4c84d8ae2f47719700c0
Wiktor Nobis <nobis171@wp.pl>
2022-06-13 12:21:04 +0200

    add react-router

---
+++ src/components/pages/Fundations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Fundations(props) {
+    return (
+        <div>
+            Fundations and organizations
+        </div>
+    );
+}
+
+export default Fundations;
---
+++ src/components/pages/WhatIs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WhatIs(props) {
+    return (
+        <div>
+            What is's about?
+        </div>
+    );
+}
+
+export default WhatIs;
