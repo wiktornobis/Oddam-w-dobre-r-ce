0c639ab3b38cf8101db599ebdbf30be33b1db01e
Wiktor Nobis <nobis171@wp.pl>
2022-06-15 08:50:44 +0200

    add routing and scroll to section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import Registration from './components/pages/Registration';
 
 function App() {
   return (
     <Router>
         <Routes>
             <Route path='/' element={<Home />} exact/>
+            <Route path='/zaloguj' element={<Login />}/>
+            <Route path='/rejestracja' element={<Registration />}/>
             
         </Routes>
         
