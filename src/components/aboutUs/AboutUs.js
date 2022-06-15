635a03ad86b240d577f215481f2334c3c7aa18fd
Wiktor Nobis <nobis171@wp.pl>
2022-06-15 09:31:26 +0200

    completed section about_us

---
+++ src/components/aboutUs/AboutUs.js
@@ -0,0 +1,22 @@
+import './_aboutUs.scss';
+import img from '../../assets/Decoration.svg';
+import img2 from '../../assets/Signature.svg';
+import img3 from '../../assets/People.jpg';
+
+function AboutUs() {
+    return (
+        <div id='o-nas' className="about">
+            <div className="about_first">
+                <h3 className="about_first_us">O nas</h3>
+                <img src={img} alt="obrazek dekoracyjny" className="about_first_img" />
+                <p className="about_first_text">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <img src={img2} alt="podpis" className="about_first_img-second" />
+            </div>
+            <div className="about_second">
+                <img src={img3} alt="grupa ludzi patrząca w dół" className="about_second_img" />
+            </div>
+        </div>
+    );
+}
+
+export default AboutUs;
--- src/components/simpleSteps/SimpleSteps.js
+++ src/components/simpleSteps/SimpleSteps.js
@@ -14,27 +14,23 @@ function SimpleSteps() {
                 <h3 className="step_first_text">Wystarczą 4 proste kroki</h3>
                 <img className="step_first_img" src={img1} alt="obrazek dekoracyjny" />
             </div>
-
             <div className="column">
                 <div className="column_first">
                     <img className="column_first_img" src={img2} alt="koszulka" />
                     <h4 className="column_first_title">Wybierz rzeczy</h4>
                     <p className="column_first_text">ubrania, zabawki, sprzęt i inne</p>
                 </div>
-
                 <div className="column_second">
                     <img className="column_second_img" src={img3} alt="paczka" />
                     <h4 className="column_second_title">Spakuj je</h4>
                     <p className="column_second_text">skorzystaj z</p>
                     <p className="column_second_text">worków na śmieci</p>
                 </div>
-
                 <div className="column_third">
                     <img className="column_third_img" src={img4} alt="lupa" />
                     <h4 className="column_third_title">Zdecyduj komu chcesz pomóc</h4>
                     <p className="column_third_text">wybierz zaufane miejsce</p>
                 </div>
-
                 <div className="column_fourth">
                     <img className="column_fourth_img" src={img5} alt="znaczek zamówienia" />
                     <h4 className="column_fourth_title">Zamów kuriera</h4>
@@ -42,15 +38,13 @@ function SimpleSteps() {
                     <p className="column_fourth_text">w dogodnym terminie</p>
                 </div>
             </div>
-
             <div className="step_second">
             <HashLink
                 to="/#zaloguj"
                 scroll={(el) => el.scrollIntoView({ behavior: 'smooth', block: 'end' })}
                 >
                 <h2 className="step_second_title">oddaj rzeczy</h2>
-            </HashLink>
-                
+            </HashLink>      
             </div>
         </div>
     );
