1abc66fb978478d8397d574b5bdb0499c2c83e0d
Wiktor Nobis <nobis171@wp.pl>
2022-06-15 15:01:01 +0200

    completed change after onClick

--- src/components/foundations/Foundations.js
+++ src/components/foundations/Foundations.js
@@ -13,59 +13,124 @@ function Foundations() {
                 <img src={img} alt="obrazek dekoracyjny" className="container_first_img" />
             </div>
             <div className="container_second">
+                
+
+
+
                 <h4 onClick={() => setActive('FirstSection')} className="container_second_text">Fundacjom</h4>
                 <h4 onClick={() => setActive('SecondSection')} className="container_second_text">Organizacjom pozarządowym</h4>
                 <h4 onClick={() => setActive('ThirdSection')} className="container_second_text">Lokalnym zbiórkom</h4>
                  
             </div>
             <div id="fundacja" className="container_third">
-            {active === 'FirstSection' && 
+
+                {active === 'FirstSection' && 
+                    <>
+                    <div className="container_third_text">
+                        <p className="container_third_text-first">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p> 
+                    </div>
+                    <div className="section">
+                        <div className="section_first">
+                            <h4 className="section_first_title">Fundacja "Dbam o Zdrowie"</h4>
+                            <h5 className="section_first_purpose">Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.</h5>
+                        </div>
+                        <div className="section_second">
+                            <p className="section_second_text">ubrania, jedzenie, sprzęt AGD, meble, zabawki</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="section_first">
+                            <h4 className="section_first_title">Fundacja “Dla dzieci”"</h4>
+                            <h5 className="section_first_purpose">Cel i misja: Pomoc dzieciom z ubogich rodzin..</h5>
+                        </div>
+                        <div className="section_second">
+                            <p className="section_second_text">ubrania, meble, zabawki</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="section_first">
+                            <h4 className="section_first_title">Fundacja “Bez domu”</h4>
+                            <h5 className="section_first_purpose">Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.</h5>
+                        </div>
+                        <div className="section_second">
+                            <p className="section_second_text">ubrania, jedzenie, ciepłe koce</p>
+                        </div>
+                    </div>
+                    </>
+                    }
+
+                    {active === 'SecondSection' && 
                 <>
                 <div className="container_third_text">
-                    <p className="container_third_text-first">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p> 
+                    <p className="container_third_text-first">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p> 
                 </div>
-
                 <div className="section">
                     <div className="section_first">
-                        <h4 className="section_first_title">Fundacja "Dbam o Zdrowie"</h4>
-                        <h5 className="section_first_purpose">Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.</h5>
+                        <h4 className="section_first_title">Organizacja “Lorem Ipsum 1”</h4>
+                        <h5 className="section_first_purpose">Quis varius quam quisque id diam vel quam elementum pulvinar.</h5>
                     </div>
                     <div className="section_second">
-                        <p className="section_second_text">ubrania, jedzenie, sprzęt AGD, meble, zabawki</p>
+                        <p className="section_second_text">Egestas, sed, tempus</p>
                     </div>
                 </div>
-
                 <div className="section">
                     <div className="section_first">
-                        <h4 className="section_first_title">Fundacja “Dla dzieci”"</h4>
-                        <h5 className="section_first_purpose">Cel i misja: Pomoc dzieciom z ubogich rodzin..</h5>
+                        <h4 className="section_first_title">Organizacja “Lorem Ipsum 2”</h4>
+                        <h5 className="section_first_purpose">Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.</h5>
                     </div>
                     <div className="section_second">
-                        <p className="section_second_text">ubrania, meble, zabawki</p>
+                        <p className="section_second_text">Ut, aliquam, purus, sit, amet</p>
                     </div>
                 </div>
-
                 <div className="section">
                     <div className="section_first">
-                        <h4 className="section_first_title">Fundacja “Bez domu”</h4>
-                        <h5 className="section_first_purpose">Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.</h5>
+                        <h4 className="section_first_title">Organizacja “Lorem Ipsum 3”</h4>
+                        <h5 className="section_first_purpose">Scelerisque in dictum non consectetur a erat nam.</h5>
                     </div>
                     <div className="section_second">
-                        <p className="section_second_text">ubrania, jedzenie, ciepłe koce</p>
+                        <p className="section_second_text">Mi, quis, hendrerit, dolor</p>
                     </div>
                 </div>
+                </> 
+                    }
 
-
-                </>
-                }
-  
-            </div>
-
-
-        
-                {active === 'SecondSection' && <p>Drugi  element</p>}
-                {active === 'ThirdSection' && <p>Trzeci element</p>}
-            
+                    {active === 'ThirdSection' && 
+                    
+                    <>
+                    <div className="container_third_text">
+                        <p className="container_third_text-first">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p> 
+                    </div>
+                    <div className="section">
+                        <div className="section_first">
+                            <h4 className="section_first_title">Zbiórka “Lorem Ipsum 1”</h4>
+                            <h5 className="section_first_purpose">Quis varius quam quisque id diam vel quam elementum pulvinar.</h5>
+                        </div>
+                        <div className="section_second">
+                            <p className="section_second_text">Egestas, sed, tempus</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="section_first">
+                            <h4 className="section_first_title">Zbiórka “Lorem Ipsum 2”</h4>
+                            <h5 className="section_first_purpose">Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.</h5>
+                        </div>
+                        <div className="section_second">
+                            <p className="section_second_text">Ut, aliquam, purus, sit, amet</p>
+                        </div>
+                    </div>
+                    <div className="section">
+                        <div className="section_first">
+                            <h4 className="section_first_title">Zbiórka “Lorem Ipsum 3”</h4>
+                            <h5 className="section_first_purpose">Scelerisque in dictum non consectetur a erat nam.</h5>
+                        </div>
+                        <div className="section_second">
+                            <p className="section_second_text">Mi, quis, hendrerit, dolor</p>
+                        </div>
+                    </div>
+                    </>
+                    }
+                
+            </div> 
         </div>
         
     );
