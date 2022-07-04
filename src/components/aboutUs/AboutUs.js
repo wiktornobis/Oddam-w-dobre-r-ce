b6d53378609b9db760452e5e098ed2b4a62699a8
Wiktor Nobis <nobis171@wp.pl>
2022-07-04 12:27:11 +0200

    rwd all section desktop and tablet completed

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -9,7 +9,10 @@ function AboutUs() {
             <div className="about_first">
                 <h3 className="about_first_us">O nas</h3>
                 <img src={img} alt="obrazek dekoracyjny" className="about_first_img" />
-                <p className="about_first_text">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <p className="about_first_text">Nori grape silver beet broccoli kombu beet 
+                                                greens fava bean potato quandong celery. Bunya nuts 
+                                                black-eyed pea prairie turnip leek lentil turnip greens parsnip.
+                </p>
                 <img src={img2} alt="podpis" className="about_first_img-second" />
             </div>
             <div className="about_second">
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -6,63 +6,64 @@ import image2 from '../../assets/Decoration.svg'
 
 function Nav() {
     return (
-        <nav className="nav">
-            <div className="left">
-            </div>
-                <div className="right">
-                <div className="right_first-column">
-                     <li id="zaloguj" className="right_first-column_text">
+        <nav className="nav_menu">
+            <div className="nav_menu_login">
+                <div className="nav_menu_login_first-column">
+                     <li id="zaloguj" className="nav_menu_login_first-column_text">
                         <Link to='/zaloguj'>Zaloguj</Link> 
                      </li>
-                     <li id="rejestracja" className="right_first-column_text-second">
+                     <li id="rejestracja" className="nav_menu_login_first-column_text-second">
                         <Link to='/rejestracja'>Załóż konto</Link> 
                      </li>
                 </div>
-                <ul className="right_second-column">
-                    <li className="right_second-column_text-decoration">
+                <ul className="nav_menu_login_second-column">
+                    <li className="nav_menu_login_second-column_text-decoration">
                         <Link to='/'>Start</Link>
                     </li>
                      <HashLink
                         to="/#o-co-chodzi"
                         scroll={(el) => el.scrollIntoView({ behavior: 'smooth', block: 'end' })}
                         >
-                        <li className="right_second-column_text">O co chodzi?</li>
+                        <li className="nav_menu_login_second-column_text">O co chodzi?</li>
                     </HashLink>
                     <HashLink
                         to="/#o-nas"
                         scroll={(el) => el.scrollIntoView({ behavior: 'smooth', block: 'end' })}
                         >
-                        <li className="right_second-column_text">O nas</li>
+                        <li className="nav_menu_login_second-column_text">O nas</li>
                     </HashLink>        
                     <HashLink
                         to="/#fundacja"
                         scroll={(el) => el.scrollIntoView({ behavior: 'smooth', block: 'end' })}
                         >
-                        <li className="right_second-column_text">Fundacja i organizacje</li>
+                        <li className="nav_menu_login_second-column_text">Fundacja i organizacje</li>
                     </HashLink>
                     <HashLink
                         to="/#kontakt"
                         scroll={(el) => el.scrollIntoView({ behavior: 'smooth', block: 'end' })}
                         >
-                        <li className="right_second-column_text">Kontakt</li>
+                        <li className="nav_menu_login_second-column_text">Kontakt</li>
                     </HashLink>
                 </ul>
-                <div className="right_text">
-                    <div className="right_third-column">
-                        <h1 className="right_third-column_text">Zacznij pomogać!</h1>
-                        <h2 className="right_third-column_text">Oddaj niechciane rzeczy w zaufane ręce</h2>
-                        <img className='right_third-column_img' src= {image2} alt="obrazek dekoracyjny" />
+            </div>
+            <div className="nav_menu_hero">
+                <div className="nav_menu_hero_left"></div>
+                <div className="nav_menu_hero_right">
+                    <div className="nav_menu_hero_third-column">
+                        <h1 className="nav_menu_hero_third-column_text">Zacznij pomogać!</h1>
+                        <h2 className="nav_menu_hero_third-column_text">Oddaj niechciane rzeczy w zaufane ręce</h2>
+                        <img className='nav_menu_hero_third-column_img' src= {image2} alt="obrazek dekoracyjny" />
                     </div>
-                    <div className="right_fourth-column">
-                        <div className="right_fourth-column-first">
+                    <div className="nav_menu_hero_fourth-column">
+                        <div className="nav_menu_hero_fourth-column-first">
                             <Link to='/zaloguj'>
-                                <li className="right_fourth-column-first_text">Oddaj</li> 
-                                <li className="right_fourth-column-first_text">rzeczy</li> 
+                                <li className="nav_menu_hero_fourth-column-first_text">Oddaj</li> 
+                                <li className="nav_menu_hero_fourth-column-first_text">rzeczy</li> 
                             </Link>
                         </div>
-                        <div className="right_fourth-column-second">
-                            <h2 className="right_fourth-column-second_text">Zorganizuj</h2>
-                            <h2 className="right_fourth-column-second_text">zbiórkę</h2>
+                        <div className="nav_menu_hero_fourth-column-second">
+                            <h2 className="nav_menu_hero_fourth-column-second_text">Zorganizuj</h2>
+                            <h2 className="nav_menu_hero_fourth-column-second_text">zbiórkę</h2>
                         </div>
                     </div>
                 </div>
