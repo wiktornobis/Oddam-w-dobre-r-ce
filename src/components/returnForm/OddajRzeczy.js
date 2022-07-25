a81b3d7e877ccf9137b307e84825289a348b62c1
Wiktor Nobis <nobis171@wp.pl>
2022-07-25 13:53:49 +0200

    add step 2 return form - unfinished

--- src/components/returnForm/OddajRzeczy.js
+++ src/components/returnForm/OddajRzeczy.js
@@ -10,12 +10,20 @@ import Contact from '../contact/Contact';
 import ContactMobile from '../contact/ContactMobile';
 
 
+
 function OddajRzeczy() {
     const [checkedOne, setCheckedOne] = useState(false)
     const [checkedTwo, setCheckedTwo] = useState(false)
     const [checkedThree, setCheckedThree] = useState(false)
     const [checkedFour, setCheckedFour] = useState(false)
     const [checkedFive, setCheckedFive] = useState(false)
+
+    const [step, setStep] = useState(0);
+
+    const [isActive, setIsActive] = useState(false);
+    const [selected, setSelected] = useState('Wybierz');
+
+    const options = ['1', '2', '3', '4', '5'];
 
     const {user, logout} = UserAuth();
     const Navigate = useNavigate();
@@ -45,6 +53,11 @@ function OddajRzeczy() {
         setCheckedFive(!checkedFive);
       };
       
+      const completeSetStep = () => {
+        setStep(cur => cur + 1)
+      }
+
+     
     return (
         <div className="return">
             <nav className="nav_menu">
@@ -131,64 +144,120 @@ function OddajRzeczy() {
                     </div>
                 </div>
             </div>
-            <div className="return_container_second">
-                <h4 className="return_container_second-title">Ważne!</h4>
-                <p className="return_container_second-paragraph">
-                Uzupełnij szczegóły dotyczące Twoich rzeczy. 
-                Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.
-                </p>
-            </div>
-            <div className="return_container_third">
-                <p className="return_container_third-paragraph">Krok 1/4</p>
-                <p className="return_container_third-title">Zaznacz co chcesz oddać</p>
-                <label className="return_container_third-label">
-                    <input
-                    className="return_container_third-input"
-                    type="checkbox"
-                    value={checkedOne}
-                    onChange={handleChangeOne}
-                    />
-                    ubrania, które nadają się do ponownego użycia
-                </label>
-                <label className="return_container_third-label">
-                    <input
-                    className="return_container_third-input"
-                    type="checkbox"
-                    value={checkedTwo}
-                    onChange={handleChangeTwo}
-                    />
-                    ubrania, do wyrzucenia
-                    <span class="checkmark"></span>
-                </label>
-                <label className="return_container_third-label">
-                    <input
-                    className="return_container_third-input"
-                    type="checkbox"
-                    value={checkedThree}
-                    onChange={handleChangeThree}
-                    />
-                    zabawki
-                </label>
-                <label className="return_container_third-label">
-                    <input
-                    className="return_container_third-input"
-                    type="checkbox"
-                    value={checkedFour}
-                    onChange={handleChangeFour}
-                    />
-                    książki
-                </label>
-                <label className="return_container_third-label">
-                    <input
-                    className="return_container_third-input"
-                    type="checkbox"
-                    value={checkedFive}
-                    onChange={handleChangeFive}
-                    />
-                    Inne
-                </label>
-                <button className="return_container_third-btn">Dalej</button>
-            </div>
+            {step === 0 && (
+                <>
+                    <div className="return_container_second">
+                        <h4 className="return_container_second-title">Ważne!</h4>
+                        <p className="return_container_second-paragraph">
+                        Uzupełnij szczegóły dotyczące Twoich rzeczy. 
+                        Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.
+                        </p>
+                    </div>
+                    <div className="return_container_third">
+                        <p className="return_container_third-paragraph">Krok 1/4</p>
+                        <p className="return_container_third-title">Zaznacz co chcesz oddać</p>
+                        <label className="return_container_third-label">
+                            <input
+                            className="return_container_third-input"
+                            type="checkbox"
+                            value={checkedOne}
+                            onChange={handleChangeOne}
+                            />
+                            ubrania, które nadają się do ponownego użycia
+                        </label>
+                        <label className="return_container_third-label">
+                            <input
+                            className="return_container_third-input"
+                            type="checkbox"
+                            value={checkedTwo}
+                            onChange={handleChangeTwo}
+                            />
+                            ubrania, do wyrzucenia
+                            <span class="checkmark"></span>
+                        </label>
+                        <label className="return_container_third-label">
+                            <input
+                            className="return_container_third-input"
+                            type="checkbox"
+                            value={checkedThree}
+                            onChange={handleChangeThree}
+                            />
+                            zabawki
+                        </label>
+                        <label className="return_container_third-label">
+                            <input
+                            className="return_container_third-input"
+                            type="checkbox"
+                            value={checkedFour}
+                            onChange={handleChangeFour}
+                            />
+                            książki
+                        </label>
+                        <label className="return_container_third-label">
+                            <input
+                            className="return_container_third-input"
+                            type="checkbox"
+                            value={checkedFive}
+                            onChange={handleChangeFive}
+                            />
+                            Inne
+                        </label>
+                        <button className="return_container_third-btn"
+                                onClick={completeSetStep}
+                        > 
+                            Dalej
+                        </button>
+                    </div>
+                </>
+            )}
+            {step === 1 && (
+                <>
+                    <div className="return_container_second">
+                        <h4 className="return_container_second-title">Ważne!</h4>
+                        <p className="return_container_second-paragraph">
+                        Wszystkie rzeczy do oddania zapakuj w 60l worki. 
+                        Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ.
+                        </p>
+                    </div>
+                    <div className="return_container_third">
+                        <p className="return_container_third-paragraph">Krok 2/4</p>
+                        <p className="return_container_third-title">Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</p>
+                        <div className="return_container_third-dropdown">
+                           <p className="return_container_third-dropdown-title"> Liczba 60l worków:</p>  
+                           <div className="return_container_third-dropdown-btn"
+                                onClick={e => setIsActive(!isActive)}
+                            >
+                                {selected}
+                                <span className="return_container_third-dropdown-down"></span>
+                            </div>
+                            {isActive && (
+                                <div className="return_container_third-dropdown-content">
+                                    {options.map((option) => (
+                                        <div className="return_container_third-dropdown-content-item"
+                                             onClick={(e) => {
+                                              setSelected(option)
+                                              setIsActive(false)
+                                            }
+                                    }   >
+                                            {option}
+                                        </div>
+
+                                    ))}
+                                        {/* <div className="return_container_third-dropdown-content-item">2</div>
+                                        <div className="return_container_third-dropdown-content-item">3</div>
+                                        <div className="return_container_third-dropdown-content-item">4</div>
+                                        <div className="return_container_third-dropdown-content-item">5</div> */}
+                                </div>
+                           )}
+                        </div>
+                        <button className="return_container_third-btn"
+                                onClick={completeSetStep}
+                        > 
+                            Dalej
+                        </button>
+                    </div>
+                </>
+            )}
             <Contact />
             <ContactMobile />
         </div>
